7fc105f16c76d7e63b36766a56653cdcd97a71f5
Matt Thornberry <mtberry16@hotmail.com>
2021-09-10 16:37:45 -0600

    Create search method

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,3 +1,34 @@
-const key = process.env.YELP_API_KEY
+const key = process.env.REACT_APP_YELP_API_KEY
 
-console.log(key)
+export const Yelp = {
+    async search(term, location, sortBy) {
+        try {
+            const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+                headers: {
+                    'Authorization': `Bearer ${key}`
+                }
+            })
+            const data = await res.json()
+            if (data.business) {
+                data.business.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.imageSrc,
+                        name: business.name,
+                        address: business.address,
+                        city: business.city,
+                        state: business.state,
+                        zipCode: business.zipCode,
+                        category: business.category,
+                        rating: business.rating,
+                        reviewCount: business.reviewCount
+                    }
+                })
+            } else {
+                throw new Error
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
